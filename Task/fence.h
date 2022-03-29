62563b1ef591e069743df762f6510b8a8b78f409
vacoo0 <101887413+vacoo0@users.noreply.github.com>
2022-03-30 01:27:11 +0200

    Add files via upload

---
+++ Task/fence.h
@@ -0,0 +1,15 @@
+#ifndef FENCE_H_INCLUDED
+#define FENCE_H_INCLUDED
+using namespace std;
+class Fence
+{
+private:
+    int a, b, area, area2, length, c, d;
+public:
+    Fence (int);
+    ~Fence();
+    void show();
+};
+
+
+#endif // FENCE_H_INCLUDED
